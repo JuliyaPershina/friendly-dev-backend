da4336e1e77083514b88f9d31a2bc8672a6feaa8
JuliyaPershina <jpershina79@gmail.com>
2026-01-09 23:39:22 +0200

    Fix admin login behind proxy (remove /admin)

--- config/server.ts
+++ config/server.ts
@@ -1,10 +1,22 @@
+// export default ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   // port: env.int('PORT', 1337),
+//   port: env.int('PORT', 3000),
+
+//   proxy: true,
+//   url: env('PUBLIC_URL'),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  // port: env.int('PORT', 1337),
   port: env.int('PORT', 3000),
 
   proxy: true,
   url: env('PUBLIC_URL'),
+
   app: {
     keys: env.array('APP_KEYS'),
   },
