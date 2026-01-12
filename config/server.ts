b1e9a7b94c643011cd190b5b535076cb75d0deb4
JuliyaPershina <jpershina79@gmail.com>
2026-01-12 13:10:04 +0200

    Fix server.ts

--- config/server.ts
+++ config/server.ts
@@ -12,10 +12,10 @@
 
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT'),
+  port: env.int('PORT',1337),
 
-  proxy: true,
-  url: env('PUBLIC_URL'),
+  // proxy: true,
+  // url: env('PUBLIC_URL'),
 
   app: {
     keys: env.array('APP_KEYS'),
